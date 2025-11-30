aa3d24a744d6bdce1a0df1e1230bf535e03a925e
Abayomi Akintomide <ywordk@gmail.com>
2025-12-01 00:55:05 +0100

    Fixed an issue where absolute filepaths were being joined to cwd in cleanFile causing error

--- src/utils/cleanFile.ts
+++ src/utils/cleanFile.ts
@@ -6,5 +6,9 @@ export async function cleanFile(
   filepath: string,
   opts: Pick<IRunnerOpts, 'cwd'>
 ) {
-  await writeFile(path.join(opts.cwd || '', filepath), '');
+  const absoluteFilepath =
+    opts.cwd && !path.isAbsolute(filepath)
+      ? path.join(opts.cwd, filepath)
+      : filepath;
+  await writeFile(absoluteFilepath, '');
 }
